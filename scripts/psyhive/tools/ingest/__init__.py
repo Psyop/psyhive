eefea72cda12ecd91e1d91df9fa87e7e69ad8dbe
Henry van der Beek <hvanderbeek@psyop.tv>
2020-09-21 12:31:08 -0700

    ingest_vendor_seqs: bugfix error on launch, | psyhive: ingest bugfix imports,

--- scripts/psyhive/tools/ingest/__init__.py
+++ scripts/psyhive/tools/ingest/__init__.py
@@ -1,11 +1,11 @@
 """Tools for ingesting file from outsource vendors."""
 
-from .ing_utils import parse_basename
+from .ing_utils import parse_basename, vendor_from_path
 from .ing_vendor_file import is_vendor_file, VendorFile
 from .ing_psy_asset import is_psy_asset, PsyAsset
 
 try:  # This will fail for outsource vendors
-    from .ing_utils_psy import vendor_from_path, ICON
+    from .ing_utils_psy import ICON
     from .ing_tools import ingest_seqs
 except ImportError:
     pass
